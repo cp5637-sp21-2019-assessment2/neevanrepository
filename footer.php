44722fe0dcfc43ef6f3266610cb7b8ac7767f264
emilbenny <46337361+emilbenny@users.noreply.github.com>
2019-05-22 17:57:08 +1000

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,52 @@
+<footer>
+    <div id="left-footer">
+        <h3>Quick Links</h3>
+        <p>
+        <ul>
+            <li>
+                <a href="<?php echo site_url(''); ?>">Home</a>
+            </li>
+            <li>
+                <a href="<?php echo site_url('/about'); ?>">About</a>
+            </li>
+            <li>
+                <a href="<?php echo site_url('/blog'); ?>">Education</a>
+            </li>
+            <li>
+                <a href="<?php echo site_url('/projects'); ?>">Sustainability</a>
+            </li>
+        </ul>
+        </p>
+    </div>
+
+    <div id="right-footer">
+        <h3>Follow us on</h3>
+        <div id="social-media">
+            <ul>
+                <li>
+                    <a href="#">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fab fa-youtube"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <p>This website is designed by team05</p>
+    </div>
+</footer>
+
+</main>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
